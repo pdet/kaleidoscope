6a20fb0da3639e89897ed8e1ef11182c1d161115
Pedro Holanda <pedroholanda@gmail.com>
2019-06-17 13:02:14 -0700

    Finishing for loop

--- src/ast/ForExprAST.cpp
+++ src/ast/ForExprAST.cpp
@@ -19,4 +19,78 @@ llvm::Value *ForExprAST::codegen(){
     // for the fall-through between two blocks.
     Builder.CreateBr(LoopBB);
 
+    //Now that the preheader of the loop is set up we start emmiting code for the body of the loop
+    //Start insertion in LoopBB
+    Builder.SetInsertPoint(LoopBB);
+
+    //Start the PHI node with an entry for Start
+    llvm::PHINode *Variable = Builder.CreatePHI(llvm::Type::getDoubleTy(TheContext),2,VarName.c_str());
+
+    Variable->addIncoming(StartVal,PreheaderBB);
+
+
+    //Within the Loop, the variable is defined equal to the PHI node. If it shadows an existing variable
+    // we have to restore it. Save it now.
+    llvm::Value *OldVal = NamedValues[VarName];
+
+    //Emit the body of the loop. This, like any other expr, can change the current BB.
+    // Note that we ignore the value computed by the body, but don't allow any error
+    if(!Body->codegen()){
+        return nullptr;
+    }
+
+    //Emit the step value.
+    llvm::Value *StepVal = nullptr;
+    if(Step) {
+        StepVal = Step->codegen();
+        if (!StepVal) {
+            return nullptr;
+        }
+    }
+    else{
+        // if not specified use 1.0
+        StepVal = llvm::ConstantFP::get(TheContext,llvm::APFloat(1.0));
+    }
+    llvm::Value * NextVar = Builder.CreateFAdd(Variable,StepVal,"nextvar");
+
+    // Compute the end condition
+    llvm::Value * EndCond = End->codegen();
+    if(!EndCond){
+        return nullptr;
+    }
+    // Convert condition to a bool by comparing non-equal to 0.0
+    EndCond = Builder.CreateFCmpONE(EndCond,llvm::ConstantFP::get(TheContext,llvm::APFloat(0.0)),"loopcond");
+
+
+    //With the code of the body of the loop complete we just need to finish up the control flow for it.
+    // This code remembers the end block (for the phi node), then creates the block for the loop exit ("afterloop")
+    // Based on the value of the exit condition it rcreates a conditional branch that chooses between executing
+    // the loop again or exiting the loop. Any future code is emitted in the "afterloop" block
+    //Create the after loop block and insert it
+    llvm::BasicBlock *LoopEndBB = Builder.GetInsertBlock();
+    llvm::BasicBlock *AfterBB = llvm::BasicBlock::Create(TheContext,"afterloop",TheFunction);
+
+    //Insert the conditional branch into the end of the LoopEndBB
+    Builder.CreateCondBr(EndCond,LoopBB,AfterBB);
+
+    //Any new code will come in AfterBB
+    Builder.SetInsertPoint(AfterBB);
+
+
+    // Now that we have the "NextVar"value we an add the incoming value to the loop phi node.
+    // After, we remove the loop variable from the symbol table so that it isn'' in the scope after the loop
+    // Finally, codegen for the loop always returns 0.0
+    //Add a new entry to the PHI node for the backend
+    Variable->addIncoming(NextVar,LoopEndBB);
+    // Restore the unshadowed variable
+    if (OldVal){
+        NamedValues[VarName] = OldVal;
+    }
+    else{
+        NamedValues.erase(VarName);
+    }
+    //for expr alawys returns 0.0
+    return llvm::Constant::getNullValue(llvm::Type::getDoubleTy(TheContext));
+
 }
+
